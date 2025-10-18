e5293317c75114034b98dab98b74011793fa1213
Kentixx <hovanskiyivan2@gmail.com>
2025-10-18 11:51:52 +0300

    feat: add Header, Button, Icon components

---
+++ src/interfaces/Button.tsx
@@ -0,0 +1,16 @@
+export interface ButtonProps {
+  className?: string;
+  href?: string;
+  type?: "button";
+  label?: string;
+  isLabelHidden?: boolean;
+  iconSrc?: string;
+  iconSize?: number;
+  ariaLabel?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+  full?: boolean;
+  fillColor?: boolean;
+  style?: React.CSSProperties;
+  hoverStyle?: React.CSSProperties;
+  activeStyle?: React.CSSProperties;
+}
