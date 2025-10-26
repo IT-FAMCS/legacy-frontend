0c71308bf872b44c486f850b7b7bb22c44e2e49d
Kentixx <hovanskiyivan2@gmail.com>
2025-10-26 20:29:47 +0300

    added some styles to Icon & Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Header.scss';
+import Icon from "../Icon";
 import Button from "../Button";
 
 const Header: React.FC = () => {
     return (
         <header className="header">
             <div className="header__inner container">
-                <Button
-                    className="header__logo"
-                    loading="eager"
-                    iconSrc="/src/assets/icons/famcs.svg"
-                    isLabelHidden
-                />
+                <a href="/">
+                    <Icon
+                        className="header__icon"
+                        loading="eager"
+                        src="/src/assets/icons/famcs.svg"
+                        isLabelHidden
+                    />
+                </a>
                 <h1 className="header__label h3">
                     {"Legacy".toUpperCase()}
                 </h1>
